b50b1c9b623eba0d18344cb77cd9456c275d3e78
Jonathan <jawhf4@mail.umkc.edu>
2020-02-12 01:03:08 -0600

    Fixed interceptor and added functionality to seach foods

--- src/app/_helpers/jwt.interceptor.ts
+++ src/app/_helpers/jwt.interceptor.ts
@@ -9,11 +9,16 @@ export class JwtInterceptor implements HttpInterceptor {
     constructor(private authenticationService: AuthenticationService) {}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if(request.url.startsWith('https://api.nutritionix.com') ||
+            request.url.startsWith('https://responsivevoice.org')) {
+            return next.handle(request);
+        }
+
         // add authorization header with jwt token if available
         let currentUser = this.authenticationService.currentUserValue;
         if (currentUser && currentUser.token) {
             request = request.clone({
-                setHeaders: { 
+                setHeaders: {
                     Authorization: `Bearer ${currentUser.token}`
                 }
             });
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({ templateUrl: 'home.component.html' })
 export class HomeComponent implements OnInit {
+    foodForm: FormGroup;
     item;
 
     constructor(
         private http: HttpClient,
+        private formBuilder: FormBuilder
     ) { }
 
     ngOnInit() {
+        this.foodForm = this.formBuilder.group({
+            FoodItem: ['', Validators.required]
+        });
     }
 
-    searchItem() {
-        //https://api.nutritionix.com/v1_1/search/McDouble?results=0:1&fields=*&appId=45876827&appKey=5e64cf8b4630c5929e1c40949be9290e
+    onSubmit() {
         const base = 'https://api.nutritionix.com/v1_1/search/';
-        const food = 'McDouble' + '?results=0:1&fields=*';
+        const food = this.foodForm.controls.FoodItem.value + '?results=0:1&fields=*';
         const appID = '&appId=45876827';
         const apiKey = '&appKey=5e64cf8b4630c5929e1c40949be9290e';
         this.http.get(base + food + appID + apiKey)
